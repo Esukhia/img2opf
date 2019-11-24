d8e9bc2749f803d951d01b8105087647dff6111f
tenzin <ten13zin@gmail.com>
2019-11-24 17:58:08 +0530

    sending OCR running and completed notification in slack

--- usage/bdrc/image_to_text.py
+++ usage/bdrc/image_to_text.py
@@ -2,11 +2,13 @@ import argparse
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 import json
+import os
 from pathlib import Path
 import requests
 
 from rdflib import URIRef
 from tqdm import tqdm
+import slack
 
 from ocr.google_ocr import get_text_from_image
 from ocr.image_list import get_volumes_for_work
@@ -16,6 +18,15 @@ from ocr.image_list import shorten
 
 
 PAGE_BREAK = '\n' + '#'*100 + '\n'
+
+# client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
+
+
+# def slack_notifier(message, msg_type='info'):
+#     response = client.chat_postMessage(
+#         channel='#google-ocr',
+#         text=f"[INFO] {message}" if msg_type=='info' else f"[ERROR] {message}"
+#    )
 
 
 def get_image(img_url):
@@ -35,6 +46,7 @@ def run_ocr(vol_id, img_info):
     img = get_image(img_url)
     response_json = get_text_from_image(img)
     response_dict = eval(response_json)
+    response_dict['image_link'] = img_url
     return response_dict
 
 
@@ -42,6 +54,8 @@ if __name__ == "__main__":
     input_path = Path('usage/bdrc/test')
     output_path = Path('usage/bdrc/output')
     output_path.mkdir(exist_ok=True)
+
+    # slack_notifier('Google OCR is running...')
 
     for workid_path in input_path.iterdir():
         # go through all the URIRef workids 
@@ -53,36 +67,41 @@ if __name__ == "__main__":
             work_dir = output_path/work_id
             work_dir.mkdir(exist_ok=True)
             # get all the volumes for the work
-            for vol_info in get_volumes_for_work(work_uri):
+            for vol_info in get_volumes_for_work(work_uri)[:1]:
                 vol_id = vol_info["volumeId"]
-
                 print(f'[INfo] volume {shorten(vol_id)} processing ....')
 
                 vol_dir = work_dir/shorten(vol_id)
                 vol_dir.mkdir(exist_ok=True)
 
                 vol_run_ocr = partial(run_ocr, vol_id)
-                # run ocr in parallel on images of the volume
-                with ProcessPoolExecutor() as executor:
-                    responses = executor.map(
-                        vol_run_ocr,
-                        get_simple_imagelist_for_vol(vol_id)
-                    )
+                try:
+                    # run ocr in parallel on images of the volume
+                    with ProcessPoolExecutor() as executor:
+                        responses = executor.map(
+                            vol_run_ocr,
+                            get_simple_imagelist_for_vol(vol_id)[:2]
+                        )
+                except:
+                    slack_notifier(f'Failed at {work_id}:{vol_id}')
                 print(f'[INFO] {shorten(vol_id)} is completed !')
 
-                text = ''
-                vol_text_fn = vol_dir/'base.txt'
-                vol_resource = vol_dir/'resource'
+                # text = ''
+                # vol_text_fn = vol_dir/'base.txt'
+                vol_resource = vol_dir/'resources'
                 vol_resource.mkdir(exist_ok=True)
                 for i, res in enumerate(responses):
                     # accumulate all the page text
                     if not res: continue # blank page response is empty
-                    page_text = res['textAnnotations'][0]['description']
-                    text += page_text + PAGE_BREAK
+                    # page_text = res['textAnnotations'][0]['description']
+                    # text += page_text + PAGE_BREAK
 
                     # save each page ocr json reponse separately
                     page_path_json = vol_resource/f'{i+1:03}.json'
                     json.dump(res, page_path_json.open('w'))
+
+    # slack_notifier('Google OCR has completed !')
+    # slack_notifier("Don't forget to stop the VM instance !")
                 
                 # save the text
-                vol_text_fn.write_text(text)
+                # vol_text_fn.write_text(text)
