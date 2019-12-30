9db22cfa49038e778bf7f5cf37c036bb6aa8b816
tenzin <ten13zin@gmail.com>
2019-12-30 16:47:25 +0530

    implement archive_on_s3

--- usage/bdrc/bdrc-ocr.py
+++ usage/bdrc/bdrc-ocr.py
@@ -1,11 +1,15 @@
 from datetime import datetime
-import io
-import os
+import gzip
 import hashlib
+import io
+import json
+import os
 from pathlib import Path
+import shutil
 
 import boto3
 import botocore
+from PIL import Image
 import requests
 import rdflib
 from rdflib import URIRef, Literal
@@ -27,7 +31,17 @@ BDR = Namespace("http://purl.bdrc.io/resource/")
 NSM = NamespaceManager(rdflib.Graph())
 NSM.bind("bdr", BDR)
 
+# s3 bucket directory config
 SERVICE = "vision"
+BATCH_PREFIX = 'batch'
+IMAGES = 'images'
+OUTPUT = 'output'
+INFO_FN = 'info.json'
+
+# local directory config
+DATA_PATH = Path('./archive')
+IMAGES_BASE_DIR = DATA_PATH/IMAGES
+OCR_BASE_DIR = DATA_PATH/OUTPUT
 
 
 def get_value(json_node):
@@ -76,7 +90,7 @@ def get_volume_infos(work_prefix_url):
 			}
 
 
-def get_s3_prefix_path(work_local_id, imagegroup):
+def get_s3_prefix_path(work_local_id, imagegroup, service=None, batch_prefix=None, data_types=None):
 	"""
 	the input is like W22084, I0886. The output is an s3 prefix ("folder"), the function
 	can be inspired from 
@@ -92,7 +106,14 @@ def get_s3_prefix_path(work_local_id, imagegroup):
 	else:
 		suffix = imagegroup
 	
-	return f'Works/{two}/{work_local_id}/images/{work_local_id}-{suffix}'
+	base_dir = f'Works/{two}/{work_local_id}'
+	if service:
+		batch_dir = f'{base_dir}/{service}/{batch_prefix}001'
+		paths = {BATCH_PREFIX: batch_dir}
+		for dt in data_types:
+			paths[dt] = f'{batch_dir}/{dt}/{work_local_id}-{suffix}'
+		return paths
+	return f'{base_dir}/images/{work_local_id}-{suffix}'
 
 
 def get_s3_bits(s3path):
@@ -113,11 +134,18 @@ def get_s3_bits(s3path):
 
 def save_file(bits, origfilename, imagegroup_output_dir):
 	"""
-	uses pillow to apply some automatic treatment and save the image.
+	uses pillow to interpret the bits as an image and save as a format
+	that is appropriate for Google Vision (png instead of tiff for instance).
+	This may also apply some automatic treatment
 	"""
-	(imagegroup_output_dir).mkdir(exist_ok=True, parents=True)
-	output_fn = imagegroup_output_dir/origfilename
-	output_fn.write_bytes(bits.getbuffer())
+	imagegroup_output_dir.mkdir(exist_ok=True, parents=True)
+	if origfilename.endswith('.tif'):
+		img = Image.open(bits)
+		output_fn = imagegroup_output_dir/f'{origfilename.split(".")[0]}.png'
+		img.save(str(output_fn))
+	else:
+		output_fn = imagegroup_output_dir/origfilename
+		output_fn.write_bytes(bits.getbuffer())
 
 
 def save_images_for_vol(volume_prefix_url, work_local_id, imagegroup, images_base_dir):
@@ -125,9 +153,8 @@ def save_images_for_vol(volume_prefix_url, work_local_id, imagegroup, images_bas
 	this function gets the list of images of a volume and download all the images from s3.
 	The output directory is output_base_dir/work_local_id/imagegroup
 	"""
-	imagelist = get_s3_image_list(volume_prefix_url)
 	s3prefix = get_s3_prefix_path(work_local_id, imagegroup)
-	for imageinfo in imagelist:
+	for imageinfo in get_s3_image_list(volume_prefix_url):
 		s3path = s3prefix+"/"+imageinfo['filename']
 		filebits = get_s3_bits(s3path)
 		imagegroup_output_dir = images_base_dir/work_local_id/imagegroup
@@ -136,14 +163,14 @@ def save_images_for_vol(volume_prefix_url, work_local_id, imagegroup, images_bas
 
 
 def gzip_str(string_):
-    # taken from https://gist.github.com/Garrett-R/dc6f08fc1eab63f94d2cbb89cb61c33d
-    out = io.BytesIO()
+	# taken from https://gist.github.com/Garrett-R/dc6f08fc1eab63f94d2cbb89cb61c33d
+	out = io.BytesIO()
 
-    with gzip.GzipFile(fileobj=out, mode='w') as fo:
-        fo.write(string_.encode())
+	with gzip.GzipFile(fileobj=out, mode='w') as fo:
+		fo.write(string_.encode())
 
-    bytes_obj = out.getvalue()
-    return bytes_obj
+	bytes_obj = out.getvalue()
+	return bytes_obj
 
 
 def apply_ocr_on_folder(images_base_dir, work_local_id, imagegroup, ocr_base_dir):
@@ -153,13 +180,13 @@ def apply_ocr_on_folder(images_base_dir, work_local_id, imagegroup, ocr_base_dir
 	"""
 	images_dir = images_base_dir/work_local_id/imagegroup
 	ocr_output_dir = ocr_base_dir/work_local_id/imagegroup
-	ocr_base_dir.mkdir(exist_ok=True, parents=True)
+	ocr_output_dir.mkdir(exist_ok=True, parents=True)
 
 	for img_fn in images_dir.iterdir():
 		result = get_text_from_image(str(img_fn))
 		gzip_result = gzip_str(result)
 
-		result_fn = ocr_output_dir/f'{img_fn.name}.json.gz'
+		result_fn = ocr_output_dir/f'{img_fn.stem}.json.gz'
 		result_fn.write_bytes(gzip_result)
 
 
@@ -176,46 +203,109 @@ def get_info_json():
 		"timestamp": {
 			"date": str(date),
 			"time": str(time).split('.')[0]	
-		}
+		},
+		'imagesfolder': IMAGES
 	}
+	
 	return info
 	
 
-def archive_on_s3(images_base_dir, ocr_base_dir, work_local_id, imagegroup):
+def archive_on_s3(images_base_dir, ocr_base_dir, work_local_id, imagegroup, s3_paths):
 	"""
 	This function uploads the images on s3, according to the schema set up by BDRC, see documentation
 	"""
+	# save info json
 	info_json = get_info_json()
-
+	s3_ocr_info_path = f'{s3_paths[BATCH_PREFIX]}/{INFO_FN}'
+	ocr_output_bucket.put_object(
+		Key=s3_ocr_info_path,
+		Body=(bytes(json.dumps(info_json).encode('UTF-8')))
+	)
+	
+	# archive images
 	images_dir = images_base_dir/work_local_id/imagegroup
 	for img_fn in images_dir.iterdir():
-		ocr_output_bucket.put_object(key=str(img_fn), Body=img_fn.read_bytes())
+		s3_image_path = f'{s3_paths[IMAGES]}/{img_fn.name}'
+		ocr_output_bucket.put_object(Key=s3_image_path, Body=img_fn.read_bytes())
 	
+	# archive ocr output
 	ocr_output_dir = ocr_base_dir/work_local_id/imagegroup
-	for out_fn in ocr_base_dir.iterdir():
-		ocr_output_bucket.put_object(key=str(out_fn), Body=out_fn.read_bytes())
+	for out_fn in ocr_output_dir.iterdir():
+		s3_output_path = f'{s3_paths[OUTPUT]}/{out_fn.name}'
+		ocr_output_bucket.put_object(Key=s3_output_path, Body=out_fn.read_bytes())
+
+
+def clean_up(data_path, work_local_id, imagegroup):
+	"""
+	delete all the images and output of the archived volume (imagegroup)
+	"""
+	vol_image_path = data_path/IMAGES/work_local_id/imagegroup
+	vol_output_path = data_path/OUTPUT/work_local_id/imagegroup
+	shutil.rmtree(str(vol_image_path))
+	shutil.rmtree(str(vol_output_path))
+
+
+def process_work(work):
+	work_local_id = work.split(':')[-1] if ':' in work else work
+
+	for vol_info in get_volume_infos(work):
+		print(f'\t[INFO] Volume {vol_info["imagegroup"]} processing ....')
+
+		# save all the images for a given vol
+		save_images_for_vol(
+			volume_prefix_url=vol_info['volume_prefix_url'],
+			work_local_id=work_local_id, 
+			imagegroup=vol_info['imagegroup'],
+			images_base_dir=IMAGES_BASE_DIR
+		)
+		print('\t\t- Saved volume images')
+
+		# apply ocr on the vol images
+		apply_ocr_on_folder(
+			images_base_dir=IMAGES_BASE_DIR,
+			work_local_id=work_local_id,
+			imagegroup=vol_info['imagegroup'],
+			ocr_base_dir=OCR_BASE_DIR
+		)
+		print('\t\t- Saved volume ocr output')
+
+		# get s3 paths to save images and ocr output
+		s3_ocr_paths = get_s3_prefix_path(
+			work_local_id=work_local_id,
+			imagegroup=vol_info['imagegroup'],
+			service=SERVICE,
+			batch_prefix=BATCH_PREFIX,
+			data_types=[IMAGES, OUTPUT]
+		)
+
+		# save image and ocr output at ocr.bdrc.org bucket
+		archive_on_s3(
+			images_base_dir=IMAGES_BASE_DIR,
+			ocr_base_dir=OCR_BASE_DIR,
+			work_local_id=work_local_id,
+			imagegroup=vol_info['imagegroup'],
+			s3_paths=s3_ocr_paths
+		)
+		print('\t\t- Archived volume images and ocr output')
+
+		# delete current ocred volume
+		clean_up(DATA_PATH, work_local_id, vol_info['imagegroup'])
+		print('\t\t- Cleaned up volume images and ocr output')
+
+		print(f'\t[INFO] Volume {vol_info["imagegroup"]} completed.')
+
+
+def get_work_ids(fn):
+	for work in fn.read_text().split('\n'):
+		if not work: continue
+		yield work.strip()
 
 
 if __name__ == "__main__":
-	# work = 'bdr:W4CZ5369'
-	# data = Path('./data')
-	# images_base_dir = data/'images'
-	# ocr_base_dir = data/'ocrs'
+	input_path = Path('usage/bdrc/input')
 
-	# for vol_info in get_volume_infos(work):
-	# 	work_local_id = work.split(':')[-1] if ':' in work else work
-		
-	# 	save_images_for_vol(
-	# 		volume_prefix_url=vol_info['volume_prefix_url'],
-	# 		work_local_id=work_local_id, 
-	# 		imagegroup=vol_info['imagegroup'],
-	# 		images_base_dir=images_base_dir
-	# 	)
-
-	# 	apply_ocr_on_folder(
-	# 		images_base_dir=images_base_dir,
-	# 		work_local_id=work_local_id,
-	# 		imagegroup=vol_info['imagegroup'],
-	# 		ocr_base_dir=ocr_base_dir
-	# 	)
-	get_info_json()
+	for workids_path in input_path.iterdir():
+		for work_id in get_work_ids(workids_path):
+			print(f'[INFO] Work {work_id} processing ....')
+			process_work(work_id)
+			print(f'[INFO] Work {work_id} completed.')
