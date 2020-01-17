a23d3084e710e5257184ae415b34471d8219bd22
tenzin <ten13zin@gmail.com>
2020-01-17 15:48:37 +0530

    separate out setup and run process

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+# set up the env
+sudo apt update
+sudo apt install python3-pip
+git clone https://github.com/Esukhia/Google-OCR.git
+pip3 install -r Google-OCR/requirements.txt
+pip3 install -e Google-OCR/
