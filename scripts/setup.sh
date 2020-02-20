4e1917150e4d3440990811a831399fd5f9c3468b
tenzin <ten13zin@gmail.com>
2020-02-20 14:49:42 +0530

    add git config in setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 # set up the env
 sudo apt update
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo apt-get install git -y
 sudo apt install python3-pip
 git clone https://github.com/Esukhia/Google-OCR.git
 pip3 install -r Google-OCR/requirements.txt
 pip3 install -e Google-OCR/
+
+# git setup
+pip3 uninstall gitdb2
+pip3 install gitdb
+git config --global user.email "ten13zin@gmail.com"
+git config --global user.name "tenzin"
