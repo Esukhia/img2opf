e39f31ca8ba3182c7b914dbd7375f5202ad53d76
tenzin <ten13zin@gmail.com>
2020-02-20 11:37:12 +0530

    installing lastest git

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 # set up the env
 sudo apt update
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get install git -y
 sudo apt install python3-pip
 git clone https://github.com/Esukhia/Google-OCR.git
 pip3 install -r Google-OCR/requirements.txt
