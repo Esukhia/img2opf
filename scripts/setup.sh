abc1a4be7eacc951b4614bc646b477fd79aa1e04
tenzin <ten13zin@gmail.com>
2020-03-11 11:27:19 +0530

    update: setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,16 @@
 # set up the env
 sudo apt update
 sudo apt install python3-pip
-git clone https://github.com/Esukhia/Google-OCR.git
+
+# install Google-OCR
+if [ -d Google-OCR ]; then
+  cd Google-OCR;
+  git pull;
+  cd ..;
+else
+  git clone https://github.com/Esukhia/Google-OCR.git;
+fi
+
 pip3 install -r Google-OCR/requirements.txt
 pip3 install -e Google-OCR/
 
