3731cbafe70ba40884050b046d15b4537be6ca15
tenzin <ten13zin@gmail.com>
2020-12-23 15:08:01 +0530

    add  extras pkgs

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
-        "openpecha",
         "google-cloud-vision==0.37.0",
-        "requests==2.22.0",
-        "boto3==1.16.41",
-        "slack-sdk==3.1.0",
     ],
+    extras_require={
+        "bdrc": [
+            "openpecha",
+            "boto3==1.16.41",
+            "slack-sdk==3.1.0",
+            "Pillow==8.0.1",
+        ]
+    },
 )
